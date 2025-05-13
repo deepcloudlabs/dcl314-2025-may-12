9914dceba36e0de267500c938740ff331344f4e6
Binnur KURT <deepcloudlabs@gmail.com>
2025-05-13 11:45:17 +0300

    Update exercise01.js

--- study-js/functional-programming/exercise01.js
+++ study-js/functional-programming/exercise01.js
@@ -1,6 +1,17 @@
 const countries = require("../resources/countries.json")
-// imperative programming
+// imperative programming: procedural programming, oop
+// external loop
+let totalPopulationOfAsia = 0;
 for (let country of countries) {
-    console.log(country.name);
+    if (country.continent === "Asia") {
+        totalPopulationOfAsia += country.population;
+    }
 }
-// declarative programming
+console.log(totalPopulationOfAsia);
+// declarative programming: functional programming, oop
+let is_asian = country => country.continent === "Asia";
+let to_population = country => country.population;
+let add = (totalPopulation, population) => totalPopulation + population;
+totalPopulationOfAsia = countries.filter(is_asian).map(to_population).reduce(add, 0)
+console.log(totalPopulationOfAsia);
+
