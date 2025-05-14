2c16450b7d6858d14aa52347a145dda6c698a7b0
Binnur KURT <deepcloudlabs@gmail.com>
2025-05-14 12:25:34 +0300

    Update exercise01.js

--- study-mongoose/exercise01.js
+++ study-mongoose/exercise01.js
@@ -4,6 +4,10 @@ const OPTIONS = {
     socketTimeoutMS: 5_000
 };
 
+function countryCodeValidator(code) {
+    return code.match(/^[A-Z]{3}$/);
+}
+
 const CitySchema = new Schema({
     _id: Number,
     name: String,
@@ -16,7 +20,7 @@ const CountrySchema = new Schema({
         required: true,
         unique: true,
         index: true,
-        regex: /^[A-Z]{3}$/
+        validate: [countryCodeValidator, "You must provide a valid country code with three letters."]
     },
     name: {
         type: String,
@@ -30,7 +34,7 @@ const CountrySchema = new Schema({
     continent: {
         type: String,
         required: true,
-        enums: [ 'Africa', 'Antarctica', 'Asia', 'Europe', 'North America', 'Oceania', 'South America']
+        enum: ['Africa', 'Antarctica', 'Asia', 'Europe', 'North America', 'Oceania', 'South America']
     },
     gnp: {
         type: Number,
@@ -64,11 +68,64 @@ const CountrySchema = new Schema({
 
 const Country = mongoose.model("countries1", CountrySchema);
 
+async function getCountryByCode(code, projection) {
+    return Country.findOne({"_id": code}, projection);
+}
 
+async function getCountryByPage(pageNo = 0, pageSize = 10, projection = {}) {
+    return Country.find({}, projection, {
+        skip: pageNo * pageSize,
+        limit: pageSize
+    });
+}
 
+async function updateCountry(country) {
+    return Country.updateOne({"_id": country._id}, country);
+}
+
+async function deleteCountry(code) {
+    return Country.findOneAndDelete({"_id": code});
+}
+
+async function getContinentCountries(continent = "Asia", pageNo = 0, pageSize = 10, projection = {}) {
+    return Country.find({continent}, projection, {
+        skip: pageNo * pageSize,
+        limit: pageSize
+    });
+}
+
+async function createCountry(country) {
+    const countryDocument = new Country(country);
+    return countryDocument.save();
+}
 
 connect("mongodb://localhost:27017/world", OPTIONS)
-    .then(() => {
-        console.log("Connected to MongoDB")
+    .then(async () => {
+        console.log("Connected to MongoDB");
+        const country = await getCountryByCode("TUR", {"cities": false});
+        console.log(country);
+        const countries = await getContinentCountries("Asia", 0, 10, {"cities": false});
+        for (const _ of countries.sort((c1, c2) => c1.name.localeCompare(c2.name)))
+            console.log(_.name);
+        /*
+        updateCountry({
+            "_id": "FFF",
+            "name": "FooCountry",
+            continent: "Asia",
+            population: 1_000,
+            surfaceArea: 1,
+            region: "FooRegion",
+            localName: "FooLocalName",
+            gnp: 1,
+            headOfState: "FooHeadOfState",
+            capital: null,
+            governmentForm: "FooGovernmentForm",
+            indepYear: 1,
+            lifeExpectancy: 1,
+            cities: []
+        }).then(result => console.log(result));
+         */
+        deleteCountry("FFF").then(result => console.log(result))
+                                 .catch(err => console.error(err));
     })
     .catch(err => console.error(err));
